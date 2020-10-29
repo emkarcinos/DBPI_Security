ca345b342273285ddec64e5d9c3955674c8f1c74
Marcin Kostrzewski <komar86311@gmail.com>
2020-10-29 13:52:41 +0100

    App now initializes connection

--- src/main/java/Main/App.java
+++ src/main/java/Main/App.java
@@ -1,13 +1,20 @@
 package Main;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
 
+
     public static void main(String[] args){
-        BasicConfigurator.configure();
+        try {
+            Connection connection = new Connection("localhost");
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }

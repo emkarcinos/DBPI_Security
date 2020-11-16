f2cea164ae79ec8f3ae714ac1b2c1ae53df622cc
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-16 12:22:08 +0100

    DH UnitTest

---
+++ src/test/java/Main/DHExchangeTest.java
@@ -0,0 +1,23 @@
+package Main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DHExchangeTest {
+
+    @Test
+    void KeyMatchAfterExchangeTest(){
+        DHExchange alice = new DHExchange(2048, true);
+        DHExchange bob = new DHExchange(2048, false);
+        alice.createKeyPar();
+        try {
+            bob.createKeyParFromPublicKey(alice.getThisPublicKeyEncoded());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        alice.onRecievePubKey(bob.getThisPublicKeyEncoded());
+
+        assertEquals(bob.getSecretKey(), alice.getSecretKey());
+    }
+}
